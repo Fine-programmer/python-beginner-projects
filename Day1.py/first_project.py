dfa82fe4f1b50971d53e83367c5eb6012584e282
Fine-programmer <faderooluwaniyimi@gmail.com>
2025-08-02 09:41:30 +0100

    Add python Day 1 file

---
+++ Day1.py/first_project.py
@@ -0,0 +1,25 @@
+# Task 1
+name = "F.I.N.E"
+age = 21
+height = "6'3"
+student = True
+
+print(name, age, height, student)
+
+# Task 2
+num1 = 10 
+num2 = 5
+add = num1 + num2
+sub = num1 * num2
+mul = num1 * num2
+div = num1 / num2
+print (add, sub, mul, div)
+
+# Task 3 (challnge)
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter another number: "))
+addition = num3 + num4
+num3_square = num3 * num3
+num4_square = num4 * num4
+remainder = num3 % num4
+print(addition, num3_square, num4_square, remainder)
